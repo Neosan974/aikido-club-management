294c717ec9c5622db93e6e394b7f49cc11a29dcb
Nicolas HOARAU <h.nico.sully@gmail.com>
2024-04-16 22:54:09 +0400

    added base template

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,3 +1,11 @@
-<template>Dashboard page. There will be stats</template>
+<template>
+  <NRow>
+    <NCol :span="2">
+      <NStatistic label="Nb of adherents">0</NStatistic>
+    </NCol>
+  </NRow>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NRow, NCol, NStatistic } from "naive-ui";
+</script>
